redmica/redmica

commit 45c1bad770bae638fc187cb1b81261e4bddc35e5

Added default value for custom fields. Fixed javascript on custom field form for project and user custom fields.
git-svn-id: https://svn.redmine.org/redmine/trunk@1090 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/custom_field.rb
@@ -53,6 +53,11 @@ class CustomField < ActiveRecord::Base
       errors.add(:possible_values, :activerecord_error_blank) if self.possible_values.nil? || self.possible_values.empty?
       errors.add(:possible_values, :activerecord_error_invalid) unless self.possible_values.is_a? Array
     end
+    
+    # validate default value
+    v = CustomValue.new(:custom_field => self.dup, :value => default_value, :customized => nil)
+    v.custom_field.is_required = false
+    errors.add(:default_value, :activerecord_error_invalid) unless v.valid?
   end
 
   def <=>(field)

app/models/custom_value.rb
@@ -19,6 +19,12 @@ class CustomValue < ActiveRecord::Base
   belongs_to :custom_field
   belongs_to :customized, :polymorphic => true
 
+  def after_initialize
+    if custom_field && new_record? && (customized_type.blank? || (customized && customized.new_record?))
+      self.value ||= custom_field.default_value
+    end
+  end
+  
 protected
   def validate
     errors.add(:value, :activerecord_error_blank) and return if custom_field.is_required? and value.blank?    

app/views/custom_fields/_form.rhtml
@@ -8,31 +8,42 @@ function toggle_custom_field_format() {
   p_regexp = $("custom_field_regexp");
   p_values = $("custom_field_possible_values");
   p_searchable = $("custom_field_searchable");
+  p_default = $("custom_field_default_value");
+  
+  p_default.setAttribute('type','text');
+  Element.show(p_default.parentNode);
+  
   switch (format.value) {
     case "list":
       Element.hide(p_length.parentNode);
       Element.hide(p_regexp.parentNode);
-      Element.show(p_searchable.parentNode);
+      if (p_searchable) Element.show(p_searchable.parentNode);
       Element.show(p_values);
       break;
-    case "date":
     case "bool":
+      p_default.setAttribute('type','checkbox');
       Element.hide(p_length.parentNode);
       Element.hide(p_regexp.parentNode);
-      Element.hide(p_searchable.parentNode);
+      if (p_searchable) Element.hide(p_searchable.parentNode);
+      Element.hide(p_values);
+      break;
+    case "date":
+      Element.hide(p_length.parentNode);
+      Element.hide(p_regexp.parentNode);
+      if (p_searchable) Element.hide(p_searchable.parentNode);
       Element.hide(p_values);
       break;
     case "float":
     case "int":
       Element.show(p_length.parentNode);
       Element.show(p_regexp.parentNode);
-      Element.hide(p_searchable.parentNode);
+      if (p_searchable) Element.hide(p_searchable.parentNode);
       Element.hide(p_values);
       break;
     default:
       Element.show(p_length.parentNode);
       Element.show(p_regexp.parentNode);
-      Element.show(p_searchable.parentNode);
+      if (p_searchable) Element.show(p_searchable.parentNode);
       Element.hide(p_values);
       break;
   }
@@ -70,6 +81,7 @@ function deleteValueField(e) {
 <span><%= text_field_tag 'custom_field[possible_values][]', value, :size => 30 %> <%= image_to_function "delete.png", "deleteValueField(this);return false" %><br /></span>
 <% end %>
 </p>
+<p><%= @custom_field.field_format == 'bool' ? f.check_box(:default_value) : f.text_field(:default_value) %></p>
 </div>
 
 <div class="box">

lang/bg.yml
@@ -161,6 +161,7 @@ field_delay: Отместване
 field_assignable: Възможно е възлагане на задачи за тази роля
 field_redirect_existing_links: Пренасочване на съществуващи линкове
 field_estimated_hours: Изчислено време
+field_default_value: Статус по подразбиране
 
 setting_app_title: Заглавие
 setting_app_subtitle: Описание

lang/cs.yml
@@ -159,6 +159,7 @@ field_is_filter: Used as a filter
 field_issue_to_id: Vztažený požadavek
 field_delay: Zpoždění
 field_assignable: Požadavky mohou být přiřazeny této roli
+field_default_value: Výchozí stav
 
 setting_app_title: Titulek aplikace
 setting_app_subtitle: Podtitulek aplikace

lang/de.yml
@@ -161,6 +161,7 @@ field_delay: Pufferzeit
 field_assignable: Tickets können dieser Rolle zugewiesen werden
 field_redirect_existing_links: Existierende Links umleiten
 field_estimated_hours: Geschätzter Aufwand
+field_default_value: Default
 
 setting_app_title: Applikations-Titel
 setting_app_subtitle: Applikations-Untertitel

lang/en.yml
@@ -174,6 +174,7 @@ field_estimated_hours: Estimated time
 field_column_names: Columns
 field_time_zone: Time zone
 field_searchable: Searchable
+field_default_value: Default value
 
 setting_app_title: Application title
 setting_app_subtitle: Application subtitle

lang/es.yml
@@ -155,6 +155,7 @@ field_identifier: Identificador
 field_is_filter: Usado como filtro
 field_issue_to_id: Petición Relacionada
 field_delay: Retraso
+field_default_value: Estado por defecto
 
 setting_app_title: Título de la aplicación
 setting_app_subtitle: Subtítulo de la aplicación

commit 05d13eb8948c0c3ae565b3ec17e87d73efe8a96a

Fixed: Issue custom fields "required" flag not stored (broken by r1090). Closes redmine-715.
git-svn-id: https://svn.redmine.org/redmine/trunk@1171 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/custom_field.rb
@@ -55,7 +55,7 @@ class CustomField < ActiveRecord::Base
     end
     
     # validate default value
-    v = CustomValue.new(:custom_field => self.dup, :value => default_value, :customized => nil)
+    v = CustomValue.new(:custom_field => self.clone, :value => default_value, :customized => nil)
     v.custom_field.is_required = false
     errors.add(:default_value, :activerecord_error_invalid) unless v.valid?
   end