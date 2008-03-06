redmica/redmica

commit 630f0de8e3f75421dbc520fe9a0a30c6c717e24f

Fixed "LdapError: invalid binding information" when no username/password are set on the LDAP account (redmine-764).
git-svn-id: https://svn.redmine.org/redmine/trunk@1194 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/auth_source_ldap.rb
@@ -70,11 +70,12 @@ class AuthSourceLdap < AuthSource
   
 private
   def initialize_ldap_con(ldap_user, ldap_password)
-    Net::LDAP.new( {:host => self.host, 
-                    :port => self.port, 
-                    :auth => { :method => :simple, :username => ldap_user, :password => ldap_password },
-                    :encryption => (self.tls ? :simple_tls : nil)} 
-    ) 
+    options = { :host => self.host,
+                :port => self.port,
+                :encryption => (self.tls ? :simple_tls : nil)
+              }
+    options.merge(:auth => { :method => :simple, :username => ldap_user, :password => ldap_password }) unless ldap_user.blank? && ldap_password.blank?
+    Net::LDAP.new options
   end
   
   def self.get_attr(entry, attr_name)

commit c9fcd50435ee94d7e2990309bb6314998a0bf664

Fix LDAP authentication (redmine-714, broken by r1194).
git-svn-id: https://svn.redmine.org/redmine/trunk@1199 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/auth_source_ldap.rb
@@ -74,7 +74,7 @@ private
                 :port => self.port,
                 :encryption => (self.tls ? :simple_tls : nil)
               }
-    options.merge(:auth => { :method => :simple, :username => ldap_user, :password => ldap_password }) unless ldap_user.blank? && ldap_password.blank?
+    options.merge!(:auth => { :method => :simple, :username => ldap_user, :password => ldap_password }) unless ldap_user.blank? && ldap_password.blank?
     Net::LDAP.new options
   end
   