redmica/redmica

commit 61194fad2f723642729ab6bc4e365cc9b7ff6974

Textile formatting: * escape html tags, except pre tags (redmine-807, redmine-795) * try to avoid unwanted quick phrase modifiers
git-svn-id: https://svn.redmine.org/redmine/trunk@1216 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redcloth.rb
@@ -295,6 +295,7 @@ class RedCloth < String
         @pre_list = []
         rip_offtags text
         no_textile text
+        escape_html_tags text
         hard_break text 
         unless @lite_mode
             refs text
@@ -375,18 +376,18 @@ class RedCloth < String
         re =
             case rtype
             when :limit
-                /(\W)
+                /(^|[>\s])
                 (#{rcq})
                 (#{C})
                 (?::(\S+?))?
-                (\S.*?\S|\S)
+                ([^\s\-].*?[^\s\-]|\w)
                 #{rcq}
-                (?=\W)/x
+                (?=[[:punct:]]|\s|$)/x
             else
                 /(#{rcq})
                 (#{C})
                 (?::(\S+))?
-                (\S.*?\S|\S)
+                ([^\s\-].*?[^\s\-]|\w)
                 #{rcq}/xm 
             end
         [rc, ht, re, rtype]
@@ -1127,6 +1128,12 @@ class RedCloth < String
                 " "
             end
         end
+    end
+    
+    ALLOWED_TAGS = %w(redpre pre)
+    
+    def escape_html_tags(text)
+      text.gsub!(%r{<((\/?)(\w+))}) {|m| ALLOWED_TAGS.include?($3) ? "<#{$1}" : "&lt;#{$1}" }
     end
 end
 

commit 17c5a6c49936a7e99c2125b45a16a495bb218fe9

Fixes redmine-880: code tags not formatted correctly in the wiki (broken by r1216).
git-svn-id: https://svn.redmine.org/redmine/trunk@1271 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redcloth.rb
@@ -1130,7 +1130,7 @@ class RedCloth < String
         end
     end
     
-    ALLOWED_TAGS = %w(redpre pre)
+    ALLOWED_TAGS = %w(redpre pre code)
     
     def escape_html_tags(text)
       text.gsub!(%r{<((\/?)(\w+))}) {|m| ALLOWED_TAGS.include?($3) ? "<#{$1}" : "&lt;#{$1}" }

test/unit/helpers/application_helper_test.rb
@@ -132,6 +132,19 @@ class ApplicationHelperTest < HelperTestCase
     to_test.each { |text, result| assert_equal "<p>#{result}</p>", textilizable(text) }
   end
   
+  def test_html_tags
+    to_test = {
+      "<div>content</div>" => "<p>&lt;div>content&lt;/div></p>",
+      "<script>some script;</script>" => "<p>&lt;script>some script;&lt;/script></p>",
+      # do not escape pre/code tags
+      "<pre>\nline 1\nline2</pre>" => "<pre>\nline 1\nline2</pre>",
+      "<pre><code>\nline 1\nline2</code></pre>" => "<pre><code>\nline 1\nline2</code></pre>",
+      "<pre><div>content</div></pre>" => "<pre>&lt;div&gt;content&lt;/div&gt;</pre>",
+    }
+    to_test.each { |text, result| assert_equal result, textilizable(text) }
+
+  end
+  
   def test_macro_hello_world
     text = "{{hello_world}}"
     assert textilizable(text).match(/Hello world!/)