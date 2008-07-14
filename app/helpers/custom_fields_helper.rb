redmica/redmica

commit 5b4b7a18f87f5ac2a28ba2d6097308de92ba5263

Custom fields refactoring: most of code moved from controllers to models (using new module ActsAsCustomizable).
git-svn-id: https://svn.redmine.org/redmine/trunk@1592 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/helpers/custom_fields_helper.rb
@@ -25,37 +25,40 @@ module CustomFieldsHelper
   end
   
   # Return custom field html tag corresponding to its format
-  def custom_field_tag(custom_value)	
+  def custom_field_tag(name, custom_value)	
     custom_field = custom_value.custom_field
-    field_name = "custom_fields[#{custom_field.id}]"
-    field_id = "custom_fields_#{custom_field.id}"
+    field_name = "#{name}[custom_field_values][#{custom_field.id}]"
+    field_id = "#{name}_custom_field_values_#{custom_field.id}"
     
     case custom_field.field_format
     when "date"
-      text_field('custom_value', 'value', :name => field_name, :id => field_id, :size => 10) + 
+      text_field_tag(field_name, custom_value.value, :id => field_id, :size => 10) + 
       calendar_for(field_id)
     when "text"
-      text_area 'custom_value', 'value', :name => field_name, :id => field_id, :rows => 3, :style => 'width:99%'
+      text_area_tag(field_name, custom_value.value, :id => field_id, :rows => 3, :style => 'width:90%')
     when "bool"
-      check_box 'custom_value', 'value', :name => field_name, :id => field_id
+      check_box_tag(field_name, custom_value.value, :id => field_id)
     when "list"
-      select 'custom_value', 'value', custom_field.possible_values, { :include_blank => true }, :name => field_name, :id => field_id
+      blank_option = custom_field.is_required? ?
+                       (custom_field.default_value.blank? ? "<option value=\"\">--- #{l(:actionview_instancetag_blank_option)} ---</option>" : '') : 
+                       '<option></option>'
+      select_tag(field_name, blank_option + options_for_select(custom_field.possible_values, custom_value.value), :id => field_id)
     else
-      text_field 'custom_value', 'value', :name => field_name, :id => field_id
+      text_field_tag(field_name, custom_value.value, :id => field_id)
     end
   end
   
   # Return custom field label tag
-  def custom_field_label_tag(custom_value)
+  def custom_field_label_tag(name, custom_value)
     content_tag "label", custom_value.custom_field.name +
 	(custom_value.custom_field.is_required? ? " <span class=\"required\">*</span>" : ""),
-	:for => "custom_fields_#{custom_value.custom_field.id}",
+	:for => "#{name}_custom_field_values_#{custom_value.custom_field.id}",
 	:class => (custom_value.errors.empty? ? nil : "error" )
   end
   
   # Return custom field tag with its label tag
-  def custom_field_tag_with_label(custom_value)
-    custom_field_label_tag(custom_value) + custom_field_tag(custom_value)
+  def custom_field_tag_with_label(name, custom_value)
+    custom_field_label_tag(name, custom_value) + custom_field_tag(name, custom_value)
   end
 
   # Return a string used to display a custom value

app/controllers/account_controller.rb
@@ -110,17 +110,12 @@ class AccountController < ApplicationController
     redirect_to(home_url) && return unless Setting.self_registration?
     if request.get?
       @user = User.new(:language => Setting.default_language)
-      @custom_values = UserCustomField.find(:all).collect { |x| CustomValue.new(:custom_field => x, :customized => @user) }
     else
       @user = User.new(params[:user])
       @user.admin = false
       @user.login = params[:user][:login]
       @user.status = User::STATUS_REGISTERED
       @user.password, @user.password_confirmation = params[:password], params[:password_confirmation]
-      @custom_values = UserCustomField.find(:all).collect { |x| CustomValue.new(:custom_field => x, 
-                                                                                :customized => @user, 
-                                                                                :value => (params["custom_fields"] ? params["custom_fields"][x.id.to_s] : nil)) }
-      @user.custom_values = @custom_values
       case Setting.self_registration
       when '1'
         # Email activation

app/controllers/issues_controller.rb
@@ -94,7 +94,6 @@ class IssuesController < ApplicationController
   end
   
   def show
-    @custom_values = @project.custom_fields_for_issues(@issue.tracker).collect { |x| @issue.custom_values.find_by_custom_field_id(x.id) || CustomValue.new(:custom_field => x, :customized => @issue) }
     @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
     @journals.each_with_index {|j,i| j.indice = i+1}
     @journals.reverse! if User.current.wants_comments_in_reverse_order?
@@ -113,15 +112,17 @@ class IssuesController < ApplicationController
   # Add a new issue
   # The new issue will be created from an existing one if copy_from parameter is given
   def new
-    @issue = params[:copy_from] ? Issue.new.copy_from(params[:copy_from]) : Issue.new(params[:issue])
+    @issue = Issue.new
+    @issue.copy_from(params[:copy_from]) if params[:copy_from]
     @issue.project = @project
-    @issue.author = User.current
     @issue.tracker ||= @project.trackers.find(params[:tracker_id] ? params[:tracker_id] : :first)
     if @issue.tracker.nil?
       flash.now[:error] = 'No tracker is associated to this project. Please check the Project settings.'
       render :nothing => true, :layout => true
       return
     end
+    @issue.attributes = params[:issue]
+    @issue.author = User.current
     
     default_status = IssueStatus.default
     unless default_status
@@ -134,17 +135,10 @@ class IssuesController < ApplicationController
     
     if request.get? || request.xhr?
       @issue.start_date ||= Date.today
-      @custom_values = @issue.custom_values.empty? ?
-        @project.custom_fields_for_issues(@issue.tracker).collect { |x| CustomValue.new(:custom_field => x, :customized => @issue) } :
-        @issue.custom_values
     else
       requested_status = IssueStatus.find_by_id(params[:issue][:status_id])
       # Check that the user is allowed to apply the requested status
       @issue.status = (@allowed_statuses.include? requested_status) ? requested_status : default_status
-      @custom_values = @project.custom_fields_for_issues(@issue.tracker).collect { |x| CustomValue.new(:custom_field => x, 
-                                                                                                       :customized => @issue,
-                                                                                                       :value => (params[:custom_fields] ? params[:custom_fields][x.id.to_s] : nil)) }
-      @issue.custom_values = @custom_values
       if @issue.save
         attach_files(@issue, params[:attachments])
         flash[:notice] = l(:notice_successful_create)
@@ -165,7 +159,6 @@ class IssuesController < ApplicationController
     @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
     @activities = Enumeration::get_values('ACTI')
     @priorities = Enumeration::get_values('IPRI')
-    @custom_values = []
     @edit_allowed = User.current.allowed_to?(:edit_issues, @project)
     
     @notes = params[:notes]
@@ -178,14 +171,7 @@ class IssuesController < ApplicationController
       @issue.attributes = attrs
     end
 
-    if request.get?
-      @custom_values = @project.custom_fields_for_issues(@issue.tracker).collect { |x| @issue.custom_values.find_by_custom_field_id(x.id) || CustomValue.new(:custom_field => x, :customized => @issue) }
-    else
-      # Update custom fields if user has :edit permission
-      if @edit_allowed && params[:custom_fields]
-        @custom_values = @project.custom_fields_for_issues(@issue.tracker).collect { |x| CustomValue.new(:custom_field => x, :customized => @issue, :value => params["custom_fields"][x.id.to_s]) }
-        @issue.custom_values = @custom_values
-      end
+    if request.post?
       @time_entry = TimeEntry.new(:project => @project, :issue => @issue, :user => User.current, :spent_on => Date.today)
       @time_entry.attributes = params[:time_entry]
       attachments = attach_files(@issue, params[:attachments])

app/controllers/projects_controller.rb
@@ -63,21 +63,17 @@ class ProjectsController < ApplicationController
   
   # Add a new project
   def add
-    @custom_fields = IssueCustomField.find(:all, :order => "#{CustomField.table_name}.position")
+    @issue_custom_fields = IssueCustomField.find(:all, :order => "#{CustomField.table_name}.position")
     @trackers = Tracker.all
     @root_projects = Project.find(:all,
                                   :conditions => "parent_id IS NULL AND status = #{Project::STATUS_ACTIVE}",
                                   :order => 'name')
     @project = Project.new(params[:project])
     if request.get?
-      @custom_values = ProjectCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| CustomValue.new(:custom_field => x, :customized => @project) }
       @project.trackers = Tracker.all
       @project.is_public = Setting.default_projects_public?
       @project.enabled_module_names = Redmine::AccessControl.available_project_modules
     else
-      @project.custom_fields = CustomField.find(params[:custom_field_ids]) if params[:custom_field_ids]
-      @custom_values = ProjectCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| CustomValue.new(:custom_field => x, :customized => @project, :value => (params[:custom_fields] ? params["custom_fields"][x.id.to_s] : nil)) }
-      @project.custom_values = @custom_values
       @project.enabled_module_names = params[:enabled_modules]
       if @project.save
         flash[:notice] = l(:notice_successful_create)
@@ -88,7 +84,6 @@ class ProjectsController < ApplicationController
 	
   # Show @project
   def show
-    @custom_values = @project.custom_values.find(:all, :include => :custom_field, :order => "#{CustomField.table_name}.position")
     @members_by_role = @project.members.find(:all, :include => [:user, :role], :order => 'position').group_by {|m| m.role}
     @subprojects = @project.children.find(:all, :conditions => Project.visible_by(User.current))
     @news = @project.news.find(:all, :limit => 5, :include => [ :author, :project ], :order => "#{News.table_name}.created_on DESC")
@@ -115,11 +110,10 @@ class ProjectsController < ApplicationController
     @root_projects = Project.find(:all,
                                   :conditions => ["parent_id IS NULL AND status = #{Project::STATUS_ACTIVE} AND id <> ?", @project.id],
                                   :order => 'name')
-    @custom_fields = IssueCustomField.find(:all)
+    @issue_custom_fields = IssueCustomField.find(:all, :order => "#{CustomField.table_name}.position")
     @issue_category ||= IssueCategory.new
     @member ||= @project.members.new
     @trackers = Tracker.all
-    @custom_values ||= ProjectCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| @project.custom_values.find_by_custom_field_id(x.id) || CustomValue.new(:custom_field => x) }
     @repository ||= @project.repository
     @wiki ||= @project.wiki
   end
@@ -127,10 +121,6 @@ class ProjectsController < ApplicationController
   # Edit @project
   def edit
     if request.post?
-      if params[:custom_fields]
-        @custom_values = ProjectCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| CustomValue.new(:custom_field => x, :customized => @project, :value => params["custom_fields"][x.id.to_s]) }
-        @project.custom_values = @custom_values
-      end
       @project.attributes = params[:project]
       if @project.save
         flash[:notice] = l(:notice_successful_update)

app/controllers/timelog_controller.rb
@@ -54,7 +54,7 @@ class TimelogController < ApplicationController
                            }
     
     # Add list and boolean custom fields as available criterias
-    @project.all_custom_fields.select {|cf| %w(list bool).include? cf.field_format }.each do |cf|
+    @project.all_issue_custom_fields.select {|cf| %w(list bool).include? cf.field_format }.each do |cf|
       @available_criterias["cf_#{cf.id}"] = {:sql => "(SELECT c.value FROM custom_values c WHERE c.custom_field_id = #{cf.id} AND c.customized_type = 'Issue' AND c.customized_id = issues.id)",
                                              :format => cf.field_format,
                                              :label => cf.name}

app/controllers/users_controller.rb
@@ -52,14 +52,11 @@ class UsersController < ApplicationController
   def add
     if request.get?
       @user = User.new(:language => Setting.default_language)
-      @custom_values = UserCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| CustomValue.new(:custom_field => x, :customized => @user) }
     else
       @user = User.new(params[:user])
       @user.admin = params[:user][:admin] || false
       @user.login = params[:user][:login]
       @user.password, @user.password_confirmation = params[:password], params[:password_confirmation] unless @user.auth_source_id
-      @custom_values = UserCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| CustomValue.new(:custom_field => x, :customized => @user, :value => (params[:custom_fields] ? params["custom_fields"][x.id.to_s] : nil)) }
-      @user.custom_values = @custom_values			
       if @user.save
         Mailer.deliver_account_information(@user, params[:password]) if params[:send_information]
         flash[:notice] = l(:notice_successful_create)
@@ -71,16 +68,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if request.get?
-      @custom_values = UserCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| @user.custom_values.find_by_custom_field_id(x.id) || CustomValue.new(:custom_field => x) }
-    else
+    if request.post?
       @user.admin = params[:user][:admin] if params[:user][:admin]
       @user.login = params[:user][:login] if params[:user][:login]
       @user.password, @user.password_confirmation = params[:password], params[:password_confirmation] unless params[:password].nil? or params[:password].empty? or @user.auth_source_id
-      if params[:custom_fields]
-        @custom_values = UserCustomField.find(:all, :order => "#{CustomField.table_name}.position").collect { |x| CustomValue.new(:custom_field => x, :customized => @user, :value => params["custom_fields"][x.id.to_s]) }
-        @user.custom_values = @custom_values
-      end
       if @user.update_attributes(params[:user])
         flash[:notice] = l(:notice_successful_update)
         # Give a string to redirect_to otherwise it would use status param as the response code

app/helpers/issues_helper.rb
@@ -149,7 +149,7 @@ module IssuesHelper
                   ]
       # Export project custom fields if project is given
       # otherwise export custom fields marked as "For all projects"
-      custom_fields = project.nil? ? IssueCustomField.for_all : project.all_custom_fields
+      custom_fields = project.nil? ? IssueCustomField.for_all : project.all_issue_custom_fields
       custom_fields.each {|f| headers << f.name}
       # Description in the last column
       headers << l(:field_description)

app/models/issue.rb
@@ -28,13 +28,12 @@ class Issue < ActiveRecord::Base
   has_many :journals, :as => :journalized, :dependent => :destroy
   has_many :attachments, :as => :container, :dependent => :destroy
   has_many :time_entries, :dependent => :delete_all
-  has_many :custom_values, :dependent => :delete_all, :as => :customized
-  has_many :custom_fields, :through => :custom_values
   has_and_belongs_to_many :changesets, :order => "revision ASC"
   
   has_many :relations_from, :class_name => 'IssueRelation', :foreign_key => 'issue_from_id', :dependent => :delete_all
   has_many :relations_to, :class_name => 'IssueRelation', :foreign_key => 'issue_to_id', :dependent => :delete_all
   
+  acts_as_customizable
   acts_as_watchable
   acts_as_searchable :columns => ['subject', "#{table_name}.description"], :include => :project, :with => {:journal => :issue}
   acts_as_event :title => Proc.new {|o| "#{o.tracker.name} ##{o.id}: #{o.subject}"},
@@ -44,7 +43,6 @@ class Issue < ActiveRecord::Base
   validates_length_of :subject, :maximum => 255
   validates_inclusion_of :done_ratio, :in => 0..100
   validates_numericality_of :estimated_hours, :allow_nil => true
-  validates_associated :custom_values, :on => :update
 
   def after_initialize
     if new_record?
@@ -52,6 +50,11 @@ class Issue < ActiveRecord::Base
       self.status ||= IssueStatus.default
       self.priority ||= Enumeration.default('IPRI')
     end
+  end
+  
+  # Overrides Redmine::Acts::Customizable::InstanceMethods#available_custom_fields
+  def available_custom_fields
+    (project && tracker) ? project.all_issue_custom_fields.select {|c| tracker.custom_fields.include? c } : []
   end
   
   def copy_from(arg)
@@ -166,11 +169,6 @@ class Issue < ActiveRecord::Base
         duplicate.update_attribute :status, self.status
       end
     end
-  end
-  
-  def custom_value_for(custom_field)
-    self.custom_values.each {|v| return v if v.custom_field_id == custom_field.id }
-    return nil
   end
   
   def init_journal(user, notes = "")

commit 9ce7f5aa385901cd8dd33bee2443eb00e03e4c2a

Fixes boolean custom fields tags (broken by r1592) (redmine-1640).
git-svn-id: https://svn.redmine.org/redmine/trunk@1668 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/helpers/custom_fields_helper.rb
@@ -37,7 +37,7 @@ module CustomFieldsHelper
     when "text"
       text_area_tag(field_name, custom_value.value, :id => field_id, :rows => 3, :style => 'width:90%')
     when "bool"
-      check_box_tag(field_name, custom_value.value, :id => field_id)
+      check_box_tag(field_name, '1', custom_value.true?, :id => field_id) + hidden_field_tag(field_name, '0')
     when "list"
       blank_option = custom_field.is_required? ?
                        (custom_field.default_value.blank? ? "<option value=\"\">--- #{l(:actionview_instancetag_blank_option)} ---</option>" : '') : 

app/models/custom_value.rb
@@ -25,6 +25,11 @@ class CustomValue < ActiveRecord::Base
     end
   end
   
+  # Returns true if the boolean custom value is true
+  def true?
+    self.value == '1'
+  end
+  
 protected
   def validate
     if value.blank?