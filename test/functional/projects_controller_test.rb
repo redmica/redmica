redmica/redmica

commit 525547ed85909daaf76c6d44d20927baa453d8c0

Activity refactoring.
git-svn-id: https://svn.redmine.org/redmine/trunk@1701 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/projects_controller.rb
@@ -226,94 +226,22 @@ class ProjectsController < ApplicationController
 
     @date_to ||= Date.today + 1
     @date_from = @date_to - @days
+    @with_subprojects = params[:with_subprojects].nil? ? Setting.display_subprojects_issues? : (params[:with_subprojects] == '1')
     
-    @event_types = %w(issues news files documents changesets wiki_pages messages)
-    if @project
-      @event_types.delete('wiki_pages') unless @project.wiki
-      @event_types.delete('changesets') unless @project.repository
-      @event_types.delete('messages') unless @project.boards.any?
-      # only show what the user is allowed to view
-      @event_types = @event_types.select {|o| User.current.allowed_to?("view_#{o}".to_sym, @project)}
-      @with_subprojects = params[:with_subprojects].nil? ? Setting.display_subprojects_issues? : (params[:with_subprojects] == '1')
-    end
-    @scope = @event_types.select {|t| params["show_#{t}"]}
-    # default events if none is specified in parameters
-    @scope = (@event_types - %w(wiki_pages messages))if @scope.empty?
-    
-    @events = []    
-    
-    if @scope.include?('issues')
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_issues, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Issue.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += Issue.find(:all, :include => [:project, :author, :tracker], :conditions => cond.conditions)
-      
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_issues, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Journal.table_name}.journalized_type = 'Issue' AND #{Journal.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      cond.add("#{JournalDetail.table_name}.prop_key = 'status_id' OR #{Journal.table_name}.notes <> ''")
-      @events += Journal.find(:all, :include => [{:issue => :project}, :details, :user], :conditions => cond.conditions)
-    end
-    
-    if @scope.include?('news')
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_news, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{News.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += News.find(:all, :include => [:project, :author], :conditions => cond.conditions)
-    end
-    
-    if @scope.include?('files')
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_files, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Attachment.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += Attachment.find(:all, :select => "#{Attachment.table_name}.*", 
-                                       :joins => "LEFT JOIN #{Version.table_name} ON #{Attachment.table_name}.container_type='Version' AND #{Version.table_name}.id = #{Attachment.table_name}.container_id " +
-                                                 "LEFT JOIN #{Project.table_name} ON #{Version.table_name}.project_id = #{Project.table_name}.id",
-                                       :conditions => cond.conditions)
-    end
-    
-    if @scope.include?('documents')
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_documents, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Document.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += Document.find(:all, :include => :project, :conditions => cond.conditions)
-      
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_documents, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Attachment.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += Attachment.find(:all, :select => "#{Attachment.table_name}.*", 
-                                       :joins => "LEFT JOIN #{Document.table_name} ON #{Attachment.table_name}.container_type='Document' AND #{Document.table_name}.id = #{Attachment.table_name}.container_id " +
-                                                 "LEFT JOIN #{Project.table_name} ON #{Document.table_name}.project_id = #{Project.table_name}.id",
-                                       :conditions => cond.conditions)
-    end
-    
-    if @scope.include?('wiki_pages')
-      select = "#{WikiContent.versioned_table_name}.updated_on, #{WikiContent.versioned_table_name}.comments, " +
-               "#{WikiContent.versioned_table_name}.#{WikiContent.version_column}, #{WikiPage.table_name}.title, " +
-               "#{WikiContent.versioned_table_name}.page_id, #{WikiContent.versioned_table_name}.author_id, " +
-               "#{WikiContent.versioned_table_name}.id"
-      joins = "LEFT JOIN #{WikiPage.table_name} ON #{WikiPage.table_name}.id = #{WikiContent.versioned_table_name}.page_id " +
-              "LEFT JOIN #{Wiki.table_name} ON #{Wiki.table_name}.id = #{WikiPage.table_name}.wiki_id " +
-              "LEFT JOIN #{Project.table_name} ON #{Project.table_name}.id = #{Wiki.table_name}.project_id"
+    @activity = Redmine::Activity::Fetcher.new(User.current, :project => @project, :with_subprojects => @with_subprojects)
+    @activity.scope_select {|t| !params["show_#{t}"].nil?}
+    @activity.default_scope! if @activity.scope.empty?
 
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_wiki_pages, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{WikiContent.versioned_table_name}.updated_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += WikiContent.versioned_class.find(:all, :select => select, :joins => joins, :conditions => cond.conditions)
-    end
-
-    if @scope.include?('changesets')
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_changesets, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Changeset.table_name}.committed_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += Changeset.find(:all, :include => {:repository => :project}, :conditions => cond.conditions)
-    end
-    
-    if @scope.include?('messages')
-      cond = ARCondition.new(Project.allowed_to_condition(User.current, :view_messages, :project => @project, :with_subprojects => @with_subprojects))
-      cond.add(["#{Message.table_name}.created_on BETWEEN ? AND ?", @date_from, @date_to])
-      @events += Message.find(:all, :include => [{:board => :project}, :author], :conditions => cond.conditions)
-    end
-    
-    @events_by_day = @events.group_by(&:event_date)
+    events = @activity.events(@date_from, @date_to)
     
     respond_to do |format|
-      format.html { render :layout => false if request.xhr? }
+      format.html { 
+        @events_by_day = events.group_by(&:event_date)
+        render :layout => false if request.xhr?
+      }
       format.atom {
         title = (@scope.size == 1) ? l("label_#{@scope.first.singularize}_plural") : l(:label_activity)
-        render_feed(@events, :title => "#{@project || Setting.app_title}: #{title}")
+        render_feed(events, :title => "#{@project || Setting.app_title}: #{title}")
       }
     end
   end

test/functional/projects_controller_test.rb
@@ -153,10 +153,6 @@ class ProjectsControllerTest < Test::Unit::TestCase
     assert_response :success
     assert_template 'activity'
     assert_not_nil assigns(:events_by_day)
-    assert_not_nil assigns(:events)
-
-    # subproject issue not included by default
-    assert !assigns(:events).include?(Issue.find(5))
     
     assert_tag :tag => "h3", 
                :content => /#{2.days.ago.to_date.day}/,
@@ -168,7 +164,9 @@ class ProjectsControllerTest < Test::Unit::TestCase
                    }
                  }
                }
-               
+  end
+  
+  def test_previous_project_activity
     get :activity, :id => 1, :from => 3.days.ago.to_date
     assert_response :success
     assert_template 'activity'
@@ -186,53 +184,24 @@ class ProjectsControllerTest < Test::Unit::TestCase
                }
   end
   
-  def test_activity_with_subprojects
-    get :activity, :id => 1, :with_subprojects => 1
-    assert_response :success
-    assert_template 'activity'
-    assert_not_nil assigns(:events)
-    
-    assert assigns(:events).include?(Issue.find(1))
-    assert !assigns(:events).include?(Issue.find(4))
-    # subproject issue
-    assert assigns(:events).include?(Issue.find(5))
-  end
-  
-  def test_global_activity_anonymous
+  def test_global_activity
     get :activity
     assert_response :success
     assert_template 'activity'
-    assert_not_nil assigns(:events)
+    assert_not_nil assigns(:events_by_day)
     
-    assert assigns(:events).include?(Issue.find(1))
-    # Issue of a private project
-    assert !assigns(:events).include?(Issue.find(4))
+    assert_tag :tag => "h3", 
+               :content => /#{5.day.ago.to_date.day}/,
+               :sibling => { :tag => "dl",
+                 :child => { :tag => "dt",
+                   :attributes => { :class => /issue/ },
+                   :child => { :tag => "a",
+                     :content => /#{Issue.find(5).subject}/,
+                   }
+                 }
+               }
   end
   
-  def test_global_activity_logged_user
-    @request.session[:user_id] = 2 # manager
-    get :activity
-    assert_response :success
-    assert_template 'activity'
-    assert_not_nil assigns(:events)
-    
-    assert assigns(:events).include?(Issue.find(1))
-    # Issue of a private project the user belongs to
-    assert assigns(:events).include?(Issue.find(4))
-  end
-
-  
-  def test_global_activity_with_all_types
-    get :activity, :show_issues => 1, :show_news => 1, :show_files => 1, :show_documents => 1, :show_changesets => 1, :show_wiki_pages => 1, :show_messages => 1
-    assert_response :success
-    assert_template 'activity'
-    assert_not_nil assigns(:events)
-    
-    assert assigns(:events).include?(Issue.find(1))
-    assert !assigns(:events).include?(Issue.find(4))
-    assert assigns(:events).include?(Message.find(5))
-  end
-
   def test_calendar
     get :calendar, :id => 1
     assert_response :success

app/models/attachment.rb
@@ -28,6 +28,18 @@ class Attachment < ActiveRecord::Base
   acts_as_event :title => :filename,
                 :url => Proc.new {|o| {:controller => 'attachments', :action => 'download', :id => o.id, :filename => o.filename}}
 
+  acts_as_activity_provider :type => 'files',
+                            :permission => :view_files,
+                            :find_options => {:select => "#{Attachment.table_name}.*", 
+                                              :joins => "LEFT JOIN #{Version.table_name} ON #{Attachment.table_name}.container_type='Version' AND #{Version.table_name}.id = #{Attachment.table_name}.container_id " +
+                                                        "LEFT JOIN #{Project.table_name} ON #{Version.table_name}.project_id = #{Project.table_name}.id"}
+  
+  acts_as_activity_provider :type => 'documents',
+                            :permission => :view_documents,
+                            :find_options => {:select => "#{Attachment.table_name}.*", 
+                                              :joins => "LEFT JOIN #{Document.table_name} ON #{Attachment.table_name}.container_type='Document' AND #{Document.table_name}.id = #{Attachment.table_name}.container_id " +
+                                                        "LEFT JOIN #{Project.table_name} ON #{Document.table_name}.project_id = #{Project.table_name}.id"}
+
   cattr_accessor :storage_path
   @@storage_path = "#{RAILS_ROOT}/files"
   

app/models/changeset.rb
@@ -30,6 +30,9 @@ class Changeset < ActiveRecord::Base
                      :include => {:repository => :project},
                      :project_key => "#{Repository.table_name}.project_id",
                      :date_column => 'committed_on'
+                     
+  acts_as_activity_provider :timestamp => "#{table_name}.committed_on",
+                            :find_options => {:include => {:repository => :project}}
   
   validates_presence_of :repository_id, :revision, :committed_on, :commit_date
   validates_uniqueness_of :revision, :scope => :repository_id

app/models/document.rb
@@ -24,7 +24,8 @@ class Document < ActiveRecord::Base
   acts_as_event :title => Proc.new {|o| "#{l(:label_document)}: #{o.title}"},
                 :author => Proc.new {|o| (a = o.attachments.find(:first, :order => "#{Attachment.table_name}.created_on ASC")) ? a.author : nil },
                 :url => Proc.new {|o| {:controller => 'documents', :action => 'show', :id => o.id}}
-
+  acts_as_activity_provider :find_options => {:include => :project}
+  
   validates_presence_of :project, :title, :category
   validates_length_of :title, :maximum => 60
 end

app/models/issue.rb
@@ -42,6 +42,8 @@ class Issue < ActiveRecord::Base
   acts_as_event :title => Proc.new {|o| "#{o.tracker.name} ##{o.id}: #{o.subject}"},
                 :url => Proc.new {|o| {:controller => 'issues', :action => 'show', :id => o.id}}                
   
+  acts_as_activity_provider :find_options => {:include => [:project, :author, :tracker]}
+  
   validates_presence_of :subject, :description, :priority, :project, :tracker, :author, :status
   validates_length_of :subject, :maximum => 255
   validates_inclusion_of :done_ratio, :in => 0..100

app/models/journal.rb
@@ -31,6 +31,12 @@ class Journal < ActiveRecord::Base
                 :type => Proc.new {|o| (s = o.new_status) ? (s.is_closed? ? 'issue-closed' : 'issue-edit') : 'issue-note' },
                 :url => Proc.new {|o| {:controller => 'issues', :action => 'show', :id => o.issue.id, :anchor => "change-#{o.id}"}}
 
+  acts_as_activity_provider :type => 'issues',
+                            :permission => :view_issues,
+                            :find_options => {:include => [{:issue => :project}, :details, :user],
+                                              :conditions => "#{Journal.table_name}.journalized_type = 'Issue' AND" +
+                                                             " (#{JournalDetail.table_name}.prop_key = 'status_id' OR #{Journal.table_name}.notes <> '')"}
+  
   def save
     # Do not save an empty journal
     (details.empty? && notes.blank?) ? false : super

app/models/message.rb
@@ -31,7 +31,9 @@ class Message < ActiveRecord::Base
                 :type => Proc.new {|o| o.parent_id.nil? ? 'message' : 'reply'},
                 :url => Proc.new {|o| {:controller => 'messages', :action => 'show', :board_id => o.board_id}.merge(o.parent_id.nil? ? {:id => o.id} : 
                                                                                                                                        {:id => o.parent_id, :anchor => "message-#{o.id}"})}
-  
+
+  acts_as_activity_provider :find_options => {:include => [{:board => :project}, :author]}
+    
   attr_protected :locked, :sticky
   validates_presence_of :subject, :content
   validates_length_of :subject, :maximum => 255

commit cd011ff6bcb3131e1ca979fcac4db6f319450ce7

Fixed: activity atom feed broken by r1701 (redmine-1703).
git-svn-id: https://svn.redmine.org/redmine/trunk@1711 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/projects_controller.rb
@@ -240,7 +240,7 @@ class ProjectsController < ApplicationController
         render :layout => false if request.xhr?
       }
       format.atom {
-        title = (@scope.size == 1) ? l("label_#{@scope.first.singularize}_plural") : l(:label_activity)
+        title = (@activity.scope.size == 1) ? l("label_#{@activity.scope.first.singularize}_plural") : l(:label_activity)
         render_feed(events, :title => "#{@project || Setting.app_title}: #{title}")
       }
     end

test/functional/projects_controller_test.rb
@@ -202,6 +202,12 @@ class ProjectsControllerTest < Test::Unit::TestCase
                }
   end
   
+  def test_activity_atom_feed
+    get :activity, :format => 'atom'
+    assert_response :success
+    assert_template 'common/feed.atom.rxml'
+  end
+  
   def test_calendar
     get :calendar, :id => 1
     assert_response :success