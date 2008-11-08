redmica/redmica

commit cadad4a089119cbf9ea0b559b11d2cfde9cf48e9

Remove pre tag attributes.
git-svn-id: https://svn.redmine.org/redmine/trunk@1930 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redcloth3.rb
@@ -1051,7 +1051,7 @@ class RedCloth3 < String
                     else
                         htmlesc( aftertag, :NoQuotes ) if aftertag
                         line = "<redpre##{ @pre_list.length }>"
-                        @pre_list << "#{ $3 }#{ aftertag }"
+                        @pre_list << "#{ $3.gsub(/<(#{ OFFTAGS })[^>]*>/, '<\\1>') }#{ aftertag }"
                     end
                 elsif $1 and codepre > 0
                     if codepre - used_offtags.length > 0

test/unit/helpers/application_helper_test.rb
@@ -181,7 +181,9 @@ class ApplicationHelperTest < HelperTestCase
       "<pre><code>\nline 1\nline2</code></pre>" => "<pre><code>\nline 1\nline2</code></pre>",
       "<pre><div>content</div></pre>" => "<pre>&lt;div&gt;content&lt;/div&gt;</pre>",
       "HTML comment: <!-- no comments -->" => "<p>HTML comment: &lt;!-- no comments --&gt;</p>",
-      "<!-- opening comment" => "<p>&lt;!-- opening comment</p>"
+      "<!-- opening comment" => "<p>&lt;!-- opening comment</p>",
+      # remove attributes
+      "<pre class='foo'>some text</pre>" => "<pre>some text</pre>",
     }
     to_test.each { |text, result| assert_equal result, textilizable(text) }
   end

commit 2c2ecb3e2bacbac1c5408bb49666c6f70d1397d9

Fixes syntax highlighting broken by r1930 (redmine-2143).
git-svn-id: https://svn.redmine.org/redmine/trunk@1993 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redcloth3.rb
@@ -1051,7 +1051,11 @@ class RedCloth3 < String
                     else
                         htmlesc( aftertag, :NoQuotes ) if aftertag
                         line = "<redpre##{ @pre_list.length }>"
-                        @pre_list << "#{ $3.gsub(/<(#{ OFFTAGS })[^>]*>/, '<\\1>') }#{ aftertag }"
+                        $3.match(/<#{ OFFTAGS }([^>]*)>/)
+                        tag = $1
+                        $2.to_s.match(/(class\=\S+)/i)
+                        tag << " #{$1}" if $1
+                        @pre_list << "<#{ tag }>#{ aftertag }"
                     end
                 elsif $1 and codepre > 0
                     if codepre - used_offtags.length > 0

test/unit/helpers/application_helper_test.rb
@@ -192,8 +192,9 @@ class ApplicationHelperTest < HelperTestCase
       "<pre><div>content</div></pre>" => "<pre>&lt;div&gt;content&lt;/div&gt;</pre>",
       "HTML comment: <!-- no comments -->" => "<p>HTML comment: &lt;!-- no comments --&gt;</p>",
       "<!-- opening comment" => "<p>&lt;!-- opening comment</p>",
-      # remove attributes
-      "<pre class='foo'>some text</pre>" => "<pre>some text</pre>",
+      # remove attributes except class
+      "<pre class='foo'>some text</pre>" => "<pre class='foo'>some text</pre>",
+      "<pre onmouseover='alert(1)'>some text</pre>" => "<pre>some text</pre>",
     }
     to_test.each { |text, result| assert_equal result, textilizable(text) }
   end
@@ -205,6 +206,21 @@ class ApplicationHelperTest < HelperTestCase
       "<notextile>this is <tag>a tag</tag></notextile>" => "this is &lt;tag&gt;a tag&lt;/tag&gt;"
     }
     to_test.each { |text, result| assert_equal result, textilizable(text) }
+  end
+  
+  def syntax_highlight
+    raw = <<-RAW
+<pre><code class="ruby">
+# Some ruby code here
+</pre></code>
+RAW
+
+    expected = <<-EXPECTED
+<pre><code class="ruby CodeRay"><span class="no">1</span> <span class="c"># Some ruby code here</span>
+</pre></code>
+EXPECTED
+
+    assert_equal expected.gsub(%r{[\r\n\t]}, ''), textilizable(raw).gsub(%r{[\r\n\t]}, '')
   end
   
   def test_wiki_links_in_tables