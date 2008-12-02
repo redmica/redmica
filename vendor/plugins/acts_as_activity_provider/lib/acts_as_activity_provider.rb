redmica/redmica

commit ccf1d5e7631976f849b34feef754f291b1bd7b28

Display latest user's activity on account/show view.
git-svn-id: https://svn.redmine.org/redmine/trunk@2066 e93f8b46-1217-0410-a6f0-8f06a7374b81

vendor/plugins/acts_as_activity_provider/lib/acts_as_activity_provider.rb
@@ -29,7 +29,7 @@ module Redmine
             send :include, Redmine::Acts::ActivityProvider::InstanceMethods
           end
 
-          options.assert_valid_keys(:type, :permission, :timestamp, :find_options)
+          options.assert_valid_keys(:type, :permission, :timestamp, :author_key, :find_options)
           self.activity_provider_options ||= {}
           
           # One model can provide different event types
@@ -39,6 +39,7 @@ module Redmine
           options[:permission] = "view_#{self.name.underscore.pluralize}".to_sym unless options.has_key?(:permission)
           options[:timestamp] ||= "#{table_name}.created_on"
           options[:find_options] ||= {}
+          options[:author_key] = "#{table_name}.#{options[:author_key]}" if options[:author_key].is_a?(Symbol)
           self.activity_provider_options[event_type] = options
         end
       end
@@ -54,10 +55,21 @@ module Redmine
             provider_options = activity_provider_options[event_type]
             raise "#{self.name} can not provide #{event_type} events." if provider_options.nil?
             
-            cond = ARCondition.new(["#{provider_options[:timestamp]} BETWEEN ? AND ?", from, to])
+            scope_options = {}
+            cond = ARCondition.new
+            if from && to
+              cond.add(["#{provider_options[:timestamp]} BETWEEN ? AND ?", from, to])
+            end
+            if options[:author]
+              return [] if provider_options[:author_key].nil?
+              cond.add(["#{provider_options[:author_key]} = ?", options[:author].id])
+            end
             cond.add(Project.allowed_to_condition(user, provider_options[:permission], options)) if provider_options[:permission]
+            scope_options[:conditions] = cond.conditions
+            scope_options[:order] = "#{provider_options[:timestamp]} DESC"
+            scope_options[:limit] = options[:limit]
             
-            with_scope(:find => { :conditions => cond.conditions }) do
+            with_scope(:find => scope_options) do
               find(:all, provider_options[:find_options])
             end
           end

app/controllers/account_controller.rb
@@ -1,5 +1,5 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2008  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -31,6 +31,10 @@ class AccountController < ApplicationController
     @memberships = @user.memberships.select do |membership|
       membership.project.is_public? || (User.current.member_of?(membership.project))
     end
+    
+    events = Redmine::Activity::Fetcher.new(User.current, :author => @user).events(nil, nil, :limit => 10)
+    @events_by_day = events.group_by(&:event_date)
+    
   rescue ActiveRecord::RecordNotFound
     render_404
   end

app/helpers/application_helper.rb
@@ -105,6 +105,18 @@ module ApplicationHelper
     @time_format ||= (Setting.time_format.blank? ? l(:general_fmt_time) : Setting.time_format)
     include_date ? local.strftime("#{@date_format} #{@time_format}") : local.strftime(@time_format)
   end
+  
+  def format_activity_title(text)
+    h(truncate_single_line(text, 100))
+  end
+  
+  def format_activity_day(date)
+    date == Date.today ? l(:label_today).titleize : format_date(date)
+  end
+  
+  def format_activity_description(text)
+    h(truncate(text.to_s, 250).gsub(%r{<(pre|code)>.*$}m, '...'))
+  end
 
   def distance_of_date_in_words(from_date, to_date = 0)
     from_date = from_date.to_date if from_date.respond_to?(:to_date)

app/helpers/projects_helper.rb
@@ -21,18 +21,6 @@ module ProjectsHelper
     link_to h(version.name), { :controller => 'versions', :action => 'show', :id => version }, options
   end
   
-  def format_activity_title(text)
-    h(truncate_single_line(text, 100))
-  end
-  
-  def format_activity_day(date)
-    date == Date.today ? l(:label_today).titleize : format_date(date)
-  end
-  
-  def format_activity_description(text)
-    h(truncate(text.to_s, 250).gsub(%r{<(pre|code)>.*$}m, '...'))
-  end
-  
   def project_settings_tabs
     tabs = [{:name => 'info', :action => :edit_project, :partial => 'projects/edit', :label => :label_information_plural},
             {:name => 'modules', :action => :select_project_modules, :partial => 'projects/settings/modules', :label => :label_module_plural},

app/models/attachment.rb
@@ -30,12 +30,14 @@ class Attachment < ActiveRecord::Base
 
   acts_as_activity_provider :type => 'files',
                             :permission => :view_files,
+                            :author_key => :author_id,
                             :find_options => {:select => "#{Attachment.table_name}.*", 
                                               :joins => "LEFT JOIN #{Version.table_name} ON #{Attachment.table_name}.container_type='Version' AND #{Version.table_name}.id = #{Attachment.table_name}.container_id " +
                                                         "LEFT JOIN #{Project.table_name} ON #{Version.table_name}.project_id = #{Project.table_name}.id"}
   
   acts_as_activity_provider :type => 'documents',
                             :permission => :view_documents,
+                            :author_key => :author_id,
                             :find_options => {:select => "#{Attachment.table_name}.*", 
                                               :joins => "LEFT JOIN #{Document.table_name} ON #{Attachment.table_name}.container_type='Document' AND #{Document.table_name}.id = #{Attachment.table_name}.container_id " +
                                                         "LEFT JOIN #{Project.table_name} ON #{Document.table_name}.project_id = #{Project.table_name}.id"}

app/models/changeset.rb
@@ -34,6 +34,7 @@ class Changeset < ActiveRecord::Base
                      :date_column => 'committed_on'
                      
   acts_as_activity_provider :timestamp => "#{table_name}.committed_on",
+                            :author_key => :user_id,
                             :find_options => {:include => {:repository => :project}}
   
   validates_presence_of :repository_id, :revision, :committed_on, :commit_date

app/models/issue.rb
@@ -42,7 +42,8 @@ class Issue < ActiveRecord::Base
   acts_as_event :title => Proc.new {|o| "#{o.tracker.name} ##{o.id}: #{o.subject}"},
                 :url => Proc.new {|o| {:controller => 'issues', :action => 'show', :id => o.id}}                
   
-  acts_as_activity_provider :find_options => {:include => [:project, :author, :tracker]}
+  acts_as_activity_provider :find_options => {:include => [:project, :author, :tracker]},
+                            :author_key => :author_id
   
   validates_presence_of :subject, :description, :priority, :project, :tracker, :author, :status
   validates_length_of :subject, :maximum => 255

app/models/journal.rb
@@ -33,6 +33,7 @@ class Journal < ActiveRecord::Base
 
   acts_as_activity_provider :type => 'issues',
                             :permission => :view_issues,
+                            :author_key => :user_id,
                             :find_options => {:include => [{:issue => :project}, :details, :user],
                                               :conditions => "#{Journal.table_name}.journalized_type = 'Issue' AND" +
                                                              " (#{JournalDetail.table_name}.prop_key = 'status_id' OR #{Journal.table_name}.notes <> '')"}

commit 2601225742de6ad61c5a01502be3238c663c1d72

Fixed: activity broken by r2066 with postgresql (redmine-2266).
git-svn-id: https://svn.redmine.org/redmine/trunk@2083 e93f8b46-1217-0410-a6f0-8f06a7374b81

vendor/plugins/acts_as_activity_provider/lib/acts_as_activity_provider.rb
@@ -66,8 +66,11 @@ module Redmine
             end
             cond.add(Project.allowed_to_condition(user, provider_options[:permission], options)) if provider_options[:permission]
             scope_options[:conditions] = cond.conditions
-            scope_options[:order] = "#{provider_options[:timestamp]} DESC"
-            scope_options[:limit] = options[:limit]
+            if options[:limit]
+              # id and creation time should be in same order in most cases
+              scope_options[:order] = "#{table_name}.id DESC"
+              scope_options[:limit] = options[:limit]
+            end
             
             with_scope(:find => scope_options) do
               find(:all, provider_options[:find_options])