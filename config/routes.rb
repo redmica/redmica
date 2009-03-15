redmica/redmica

commit e61bb516625dc79536f1db0c5babd323db534c90

Fixing repository routes (redmine-2967).
git-svn-id: https://svn.redmine.org/redmine/trunk@2581 e93f8b46-1217-0410-a6f0-8f06a7374b81

config/routes.rb
@@ -209,16 +209,16 @@ ActionController::Routing::Routes.draw do |map|
   
   map.with_options :controller => 'repositories' do |repositories|
     repositories.with_options :conditions => {:method => :get} do |repository_views|
-      repositories.connect 'projects/:id/repository', :action => 'show'
-      repositories.connect 'projects/:id/repository/edit', :action => 'edit'
-      repositories.connect 'projects/:id/repository/statistics', :action => 'stats'
-      repositories.connect 'projects/:id/repository/revisions', :action => 'revisions'
-      repositories.connect 'projects/:id/repository/revisions.:format', :action => 'revisions'
-      repositories.connect 'projects/:id/repository/revisions/:rev', :action => 'revision'
-      repositories.connect 'projects/:id/repository/revisions/:rev/diff', :action => 'diff'
-      repositories.connect 'projects/:id/repository/revisions/:rev/diff.:format', :action => 'diff'
-      repositories.connect 'projects/:id/repository/revisions/:rev/:action/*path'
-      repositories.connect 'projects/:id/repository/:action/*path'
+      repository_views.connect 'projects/:id/repository', :action => 'show'
+      repository_views.connect 'projects/:id/repository/edit', :action => 'edit'
+      repository_views.connect 'projects/:id/repository/statistics', :action => 'stats'
+      repository_views.connect 'projects/:id/repository/revisions', :action => 'revisions'
+      repository_views.connect 'projects/:id/repository/revisions.:format', :action => 'revisions'
+      repository_views.connect 'projects/:id/repository/revisions/:rev', :action => 'revision'
+      repository_views.connect 'projects/:id/repository/revisions/:rev/diff', :action => 'diff'
+      repository_views.connect 'projects/:id/repository/revisions/:rev/diff.:format', :action => 'diff'
+      repository_views.connect 'projects/:id/repository/revisions/:rev/:action/*path'
+      repository_views.connect 'projects/:id/repository/:action/*path'
     end
     
     repositories.connect 'projects/:id/repository/edit', :action => 'edit', :conditions => {:method => :post}

app/views/repositories/_dir_list_content.rhtml
@@ -5,6 +5,7 @@
 <td style="padding-left: <%=18 * depth%>px;" class="filename">
 <% if entry.is_dir? %>
 <span class="expander" onclick="<%=  remote_function :url => {:action => 'browse', :id => @project, :path => to_path_param(entry.path), :rev => @rev, :depth => (depth + 1), :parent_id => tr_id},
+									:method => :get,
                   :update => { :success => tr_id },
                   :position => :after,
                   :success => "scmEntryLoaded('#{tr_id}')",

app/views/repositories/browse.rhtml
@@ -1,5 +1,5 @@
 <div class="contextual">
-<% form_tag do %>
+<% form_tag({}, :method => :get) do %>
 <%= l(:label_revision) %>: <%= text_field_tag 'rev', @rev, :size => 5 %>
 <% end %>
 </div>

app/views/repositories/show.rhtml
@@ -3,7 +3,7 @@
 <%= link_to l(:label_statistics), {:action => 'stats', :id => @project}, :class => 'icon icon-stats' %>
 
 <% if !@entries.nil? && authorize_for('repositories', 'browse') -%>
-<% form_tag(:action => 'browse', :id => @project) do -%>
+<% form_tag({:action => 'browse', :id => @project}, :method => :get) do -%>
 | <%= l(:label_revision) %>: <%= text_field_tag 'rev', @rev, :size => 5 %>
 <% end -%>
 <% end -%>

commit fc0c6fc21c718ed484370f9df2d3c342a3cf9cfe

Fixing routes broken by r2581 (redmine-2967, redmine-2970).
git-svn-id: https://svn.redmine.org/redmine/trunk@2590 e93f8b46-1217-0410-a6f0-8f06a7374b81

config/routes.rb
@@ -221,7 +221,7 @@ ActionController::Routing::Routes.draw do |map|
       repository_views.connect 'projects/:id/repository/:action/*path'
     end
     
-    repositories.connect 'projects/:id/repository/edit', :action => 'edit', :conditions => {:method => :post}
+    repositories.connect 'projects/:id/repository/:action', :conditions => {:method => :post}
   end
   
   map.connect 'attachments/:id', :controller => 'attachments', :action => 'show', :id => /\d+/