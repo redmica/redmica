redmica/redmica

commit c3bb3a115a675d8998d233fad0791e2ae2704335

Allows multiple roles on the same project (redmine-706). Prerequisite for user groups feature.
git-svn-id: https://svn.redmine.org/redmine/trunk@2726 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -146,7 +146,7 @@ class IssuesController < ApplicationController
       return
     end    
     @issue.status = default_status
-    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.role_for_project(@project), @issue.tracker)).uniq
+    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.roles_for_project(@project), @issue.tracker)).uniq
     
     if request.get? || request.xhr?
       @issue.start_date ||= Date.today

test/functional/issues_controller_test.rb
@@ -26,6 +26,7 @@ class IssuesControllerTest < Test::Unit::TestCase
            :users,
            :roles,
            :members,
+           :member_roles,
            :issues,
            :issue_statuses,
            :versions,

commit 2cb3da65bd93ce0570120008e0e699be5faaa4f8

Fixed: issue bulk edit view broken by r2726 (redmine-3347).
git-svn-id: https://svn.redmine.org/redmine/trunk@2739 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -273,7 +273,7 @@ class IssuesController < ApplicationController
     end
     # Find potential statuses the user could be allowed to switch issues to
     @available_statuses = Workflow.find(:all, :include => :new_status,
-                                              :conditions => {:role_id => current_role.id}).collect(&:new_status).compact.uniq.sort
+                                              :conditions => {:role_id => User.current.roles_for_project(@project).collect(&:id)}).collect(&:new_status).compact.uniq.sort
     @custom_fields = @project.issue_custom_fields.select {|f| f.field_format == 'list'}
   end
 

test/functional/issues_controller_test.rb
@@ -831,6 +831,13 @@ class IssuesControllerTest < Test::Unit::TestCase
                           :content => notes
     assert_tag :input, :attributes => { :name => 'time_entry[hours]', :value => "2z" }
   end
+  
+  def test_get_bulk_edit
+    @request.session[:user_id] = 2
+    get :bulk_edit, :ids => [1, 2]
+    assert_response :success
+    assert_template 'bulk_edit'
+  end
 
   def test_bulk_edit
     @request.session[:user_id] = 2