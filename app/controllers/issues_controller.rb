redmica/redmica

commit c3bb3a115a675d8998d233fad0791e2ae2704335

Allows multiple roles on the same project (redmine-706). Prerequisite for user groups feature.
git-svn-id: https://svn.redmine.org/redmine/trunk@2726 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -146,7 +146,7 @@ class IssuesController < ApplicationController
       return
     end    
     @issue.status = default_status
-    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.role_for_project(@project), @issue.tracker)).uniq
+    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.roles_for_project(@project), @issue.tracker)).uniq
     
     if request.get? || request.xhr?
       @issue.start_date ||= Date.today

test/functional/issues_controller_test.rb
@@ -26,6 +26,7 @@ class IssuesControllerTest < Test::Unit::TestCase
            :users,
            :roles,
            :members,
+           :member_roles,
            :issues,
            :issue_statuses,
            :versions,

commit 63b66384fbf98c7aee42b13cdf2462a5735e2dc4

Fixed: issue move by non-admin broken by r2726 (redmine-3354).
git-svn-id: https://svn.redmine.org/redmine/trunk@2738 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -284,7 +284,7 @@ class IssuesController < ApplicationController
       # admin is allowed to move issues to any active (visible) project
       @allowed_projects = Project.find(:all, :conditions => Project.visible_by(User.current))
     else
-      User.current.memberships.each {|m| @allowed_projects << m.project if m.role.allowed_to?(:move_issues)}
+      User.current.memberships.each {|m| @allowed_projects << m.project if m.roles.detect {|r| r.allowed_to?(:move_issues)}}
     end
     @target_project = @allowed_projects.detect {|p| p.id.to_s == params[:new_project_id]} if params[:new_project_id]
     @target_project ||= @project    

test/functional/issues_controller_test.rb
@@ -905,14 +905,14 @@ class IssuesControllerTest < Test::Unit::TestCase
   end
   
   def test_move_one_issue_to_another_project
-    @request.session[:user_id] = 1
+    @request.session[:user_id] = 2
     post :move, :id => 1, :new_project_id => 2
     assert_redirected_to :action => 'index', :project_id => 'ecookbook'
     assert_equal 2, Issue.find(1).project_id
   end
 
   def test_bulk_move_to_another_project
-    @request.session[:user_id] = 1
+    @request.session[:user_id] = 2
     post :move, :ids => [1, 2], :new_project_id => 2
     assert_redirected_to :action => 'index', :project_id => 'ecookbook'
     # Issues moved to project 2
@@ -924,7 +924,7 @@ class IssuesControllerTest < Test::Unit::TestCase
   end
  
   def test_bulk_move_to_another_tracker
-    @request.session[:user_id] = 1
+    @request.session[:user_id] = 2
     post :move, :ids => [1, 2], :new_tracker_id => 2
     assert_redirected_to :action => 'index', :project_id => 'ecookbook'
     assert_equal 2, Issue.find(1).tracker_id
@@ -932,7 +932,7 @@ class IssuesControllerTest < Test::Unit::TestCase
   end
 
   def test_bulk_copy_to_another_project
-    @request.session[:user_id] = 1
+    @request.session[:user_id] = 2
     assert_difference 'Issue.count', 2 do
       assert_no_difference 'Project.find(1).issues.count' do
         post :move, :ids => [1, 2], :new_project_id => 2, :copy_options => {:copy => '1'}