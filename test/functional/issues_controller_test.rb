redmica/redmica

commit c3bb3a115a675d8998d233fad0791e2ae2704335

Allows multiple roles on the same project (redmine-706). Prerequisite for user groups feature.
git-svn-id: https://svn.redmine.org/redmine/trunk@2726 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -146,7 +146,7 @@ class IssuesController < ApplicationController
       return
     end    
     @issue.status = default_status
-    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.role_for_project(@project), @issue.tracker)).uniq
+    @allowed_statuses = ([default_status] + default_status.find_new_statuses_allowed_to(User.current.roles_for_project(@project), @issue.tracker)).uniq
     
     if request.get? || request.xhr?
       @issue.start_date ||= Date.today

test/functional/issues_controller_test.rb
@@ -26,6 +26,7 @@ class IssuesControllerTest < Test::Unit::TestCase
            :users,
            :roles,
            :members,
+           :member_roles,
            :issues,
            :issue_statuses,
            :versions,

commit 21806c73cc78a55478a23ef35afb468428f0a9b6

Fixed: issue status bulk edit broken by r2726 (redmine-3347).
git-svn-id: https://svn.redmine.org/redmine/trunk@2744 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -256,7 +256,7 @@ class IssuesController < ApplicationController
         issue.custom_field_values = custom_field_values if custom_field_values && !custom_field_values.empty?
         call_hook(:controller_issues_bulk_edit_before_save, { :params => params, :issue => issue })
         # Don't save any change to the issue if the user is not authorized to apply the requested status
-        unless (status.nil? || (issue.status.new_status_allowed_to?(status, current_role, issue.tracker) && issue.status = status)) && issue.save
+        unless (status.nil? || (issue.new_statuses_allowed_to(User.current).include?(status) && issue.status = status)) && issue.save
           # Keep unsaved issue ids to display them in flash error
           unsaved_issue_ids << issue.id
         end

test/functional/issues_controller_test.rb
@@ -873,6 +873,18 @@ class IssuesControllerTest < Test::Unit::TestCase
     assert_equal 2, ActionMailer::Base.deliveries.size
   end
 
+  def test_bulk_edit_status
+    @request.session[:user_id] = 2
+    # update issues priority
+    post :bulk_edit, :ids => [1, 2], :priority_id => '',
+                                     :assigned_to_id => '',
+                                     :status_id => '5',
+                                     :notes => 'Bulk editing status'
+    assert_response 302
+    issue = Issue.find(1)
+    assert issue.closed?
+  end
+
   def test_bulk_edit_custom_field
     @request.session[:user_id] = 2
     # update issues priority