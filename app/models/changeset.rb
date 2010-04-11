redmica/redmica

commit 1794e656df83105e25780f1e8dc338f2edd4149b

Remove invalid utf8 sequences from commit comments and author name (redmine-4773).
git-svn-id: https://svn.redmine.org/redmine/trunk@3466 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/changeset.rb
@@ -1,5 +1,5 @@
 # Redmine - project management software
-# Copyright (C) 2006-2008  Jean-Philippe Lang
+# Copyright (C) 2006-2010  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -57,6 +57,10 @@ class Changeset < ActiveRecord::Base
     super
   end
   
+  def committer=(arg)
+    write_attribute(:committer, self.class.to_utf8(arg.to_s))
+  end
+
   def project
     repository.project
   end
@@ -180,11 +184,12 @@ class Changeset < ActiveRecord::Base
     encoding = Setting.commit_logs_encoding.to_s.strip
     unless encoding.blank? || encoding == 'UTF-8'
       begin
-        return Iconv.conv('UTF-8', encoding, str)
+        str = Iconv.conv('UTF-8', encoding, str)
       rescue Iconv::Failure
         # do nothing here
       end
     end
-    str
+    # removes invalid UTF8 sequences
+    Iconv.conv('UTF-8//IGNORE', 'UTF-8', str + '  ')[0..-3]
   end
 end

test/unit/changeset_test.rb
@@ -1,5 +1,7 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# encoding: utf-8
+#
+# Redmine - project management software
+# Copyright (C) 2006-2010  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -116,5 +118,19 @@ class ChangesetTest < ActiveSupport::TestCase
   def test_next_nil
     changeset = Changeset.find_by_revision('10')
     assert_nil changeset.next
+  end
+  
+  def test_comments_should_be_converted_to_utf8
+    with_settings :commit_logs_encoding => 'ISO-8859-1' do
+      c = Changeset.new
+      c.comments = File.read("#{RAILS_ROOT}/test/fixtures/encoding/iso-8859-1.txt")
+      assert_equal "Texte encodé en ISO-8859-1.", c.comments
+    end
+  end
+  
+  def test_invalid_utf8_sequences_in_comments_should_be_stripped
+    c = Changeset.new
+    c.comments = File.read("#{RAILS_ROOT}/test/fixtures/encoding/iso-8859-1.txt")
+    assert_equal "Texte encod en ISO-8859-1.", c.comments
   end
 end

commit a0b534afb6cd68a276cbea9957fc587ac31bb93c

Fixed: Update of Subversion changesets broken by r3466 under Solaris (redmine-5255).
git-svn-id: https://svn.redmine.org/redmine/trunk@3635 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/changeset.rb
@@ -191,6 +191,11 @@ class Changeset < ActiveRecord::Base
       end
     end
     # removes invalid UTF8 sequences
-    Iconv.conv('UTF-8//IGNORE', 'UTF-8', str + '  ')[0..-3]
+    begin
+      Iconv.conv('UTF-8//IGNORE', 'UTF-8', str + '  ')[0..-3]
+    rescue Iconv::InvalidEncoding
+      # "UTF-8//IGNORE" is not supported on some OS
+      str
+    end
   end
 end