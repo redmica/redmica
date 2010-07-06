redmica/redmica

commit 52876b8733ab809c67919994bcbf44381fc02891

Extend changes.path and changes.from_path to support longer paths. #5771
Long paths to SCM files would be truncated or wouldn't insert into some
databases.  This extends those fields to support arbitrary length filenames.

Contributed by Holger Just

git-svn-id: https://svn.redmine.org/redmine/trunk@3828 e93f8b46-1217-0410-a6f0-8f06a7374b81

db/migrate/20100705164950_change_changes_path_length_limit.rb
@@ -1,0 +1,11 @@
+class ChangeChangesPathLengthLimit < ActiveRecord::Migration
+  def self.up
+    change_column :changes, :path, :text, :default => "", :null => false
+    change_column :changes, :from_path, :text
+  end
+
+  def self.down
+    change_column :changes, :path, :string, :default => "", :null => false
+    change_column :changes, :from_path, :string
+  end
+end

test/functional/repositories_git_controller_test.rb
@@ -50,8 +50,9 @@ class RepositoriesGitControllerTest < ActionController::TestCase
       assert_response :success
       assert_template 'show'
       assert_not_nil assigns(:entries)
-      assert_equal 6, assigns(:entries).size
+      assert_equal 7, assigns(:entries).size
       assert assigns(:entries).detect {|e| e.name == 'images' && e.kind == 'dir'}
+      assert assigns(:entries).detect {|e| e.name == 'this_is_a_really_long_and_verbose_directory_name' && e.kind == 'dir'}
       assert assigns(:entries).detect {|e| e.name == 'sources' && e.kind == 'dir'}
       assert assigns(:entries).detect {|e| e.name == 'README' && e.kind == 'file'}
       assert assigns(:entries).detect {|e| e.name == 'copied_README' && e.kind == 'file'}

test/unit/lib/redmine/scm/adapters/git_adapter_test.rb
@@ -13,7 +13,7 @@ class GitAdapterTest < ActiveSupport::TestCase
     end
 
     def test_getting_all_revisions
-      assert_equal 12, @adapter.revisions('',nil,nil,:all => true).length
+      assert_equal 13, @adapter.revisions('',nil,nil,:all => true).length
     end
     
     def test_annotate

test/unit/repository_git_test.rb
@@ -34,8 +34,8 @@ class RepositoryGitTest < ActiveSupport::TestCase
       @repository.fetch_changesets
       @repository.reload
       
-      assert_equal 12, @repository.changesets.count
-      assert_equal 21, @repository.changes.count
+      assert_equal 13, @repository.changesets.count
+      assert_equal 22, @repository.changes.count
       
       commit = @repository.changesets.find(:first, :order => 'committed_on ASC')
       assert_equal "Initial import.\nThe repository contains 3 files.", commit.comments
@@ -57,10 +57,10 @@ class RepositoryGitTest < ActiveSupport::TestCase
       # Remove the 3 latest changesets
       @repository.changesets.find(:all, :order => 'committed_on DESC', :limit => 3).each(&:destroy)
       @repository.reload
-      assert_equal 9, @repository.changesets.count
+      assert_equal 10, @repository.changesets.count
       
       @repository.fetch_changesets
-      assert_equal 12, @repository.changesets.count
+      assert_equal 13, @repository.changesets.count
     end
   else
     puts "Git test repository NOT FOUND. Skipping unit tests !!!"

commit 06d12f471d739ec91738c469f1c99fb29f71f55b

Force the default value of path to be set on the Change model class. #5771
This is needed because MySQL does not support default values on text columns
(Error introduced in r3828)

Contributed by Holger Just

git-svn-id: https://svn.redmine.org/redmine/trunk@3830 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/change.rb
@@ -23,4 +23,8 @@ class Change < ActiveRecord::Base
   def relative_path
     changeset.repository.relative_path(path)
   end
+  
+  def before_save
+    path ||= ""
+  end
 end

db/migrate/20100705164950_change_changes_path_length_limit.rb
@@ -1,6 +1,6 @@
 class ChangeChangesPathLengthLimit < ActiveRecord::Migration
   def self.up
-    change_column :changes, :path, :text, :default => "", :null => false
+    change_column :changes, :path, :text, :null => false
     change_column :changes, :from_path, :text
   end
 