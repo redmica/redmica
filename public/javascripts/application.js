redmica/redmica

commit 7516ce8573c74fa2473cc28b06a4ca60db89aef8

Disable submit buttons when submitting a form (redmine-6555).
git-svn-id: https://svn.redmine.org/redmine/trunk@9391 e93f8b46-1217-0410-a6f0-8f06a7374b81

public/javascripts/application.js
@@ -520,4 +520,16 @@ function hideOnLoad() {
   });
 }
 
+function addFormObserversForDoubleSubmit() {
+  $$('form[method=post]').each(function(el) {
+    Event.observe(el, 'submit', function(e) {
+      var form = Event.element(e);
+      form.select('input[type=submit]').each(function(btn) {
+        btn.disable();
+      });
+    });
+  });
+}
+
 Event.observe(window, 'load', hideOnLoad);
+Event.observe(window, 'load', addFormObserversForDoubleSubmit);

commit a79b69b13cd8e5c580b76a031f2d1e4e7b57f325

Fixed that "Create and continue" buttons are broken by r9391 (redmine-10675).
git-svn-id: https://svn.redmine.org/redmine/trunk@9442 e93f8b46-1217-0410-a6f0-8f06a7374b81

public/javascripts/application.js
@@ -521,13 +521,16 @@ function hideOnLoad() {
 }
 
 function addFormObserversForDoubleSubmit() {
-  $$('form[method=post]').each(function(el) {
-    Event.observe(el, 'submit', function(e) {
-      var form = Event.element(e);
-      form.select('input[type=submit]').each(function(btn) {
-        btn.disable();
+  $$('form[method=post]').each(function(form) {
+    if (!form.hasClassName('multiple-submit')) {
+      form.on('submit', function(form_submission) { 
+        if (form.getStorage().get('submitted')) {
+          form_submission.stop();
+        } else {
+          form.getStorage().set('submitted', true);
+        }
       });
-    });
+    }
   });
 }
 