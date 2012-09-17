redmica/redmica

commit 7e2e8fb657c87ff4da82fc77e334292401dc36e3

Use named routes.
git-svn-id: https://svn.redmine.org/redmine/trunk@9940 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/account_controller.rb
@@ -70,7 +70,7 @@ class AccountController < ApplicationController
         if token.save
           Mailer.lost_password(token).deliver
           flash[:notice] = l(:notice_account_lost_email_sent)
-          redirect_to :action => 'login'
+          redirect_to signin_path
           return
         end
       end
@@ -129,7 +129,7 @@ class AccountController < ApplicationController
       token.destroy
       flash[:notice] = l(:notice_account_activated)
     end
-    redirect_to :action => 'login'
+    redirect_to signin_path
   end
 
   private
@@ -224,7 +224,7 @@ class AccountController < ApplicationController
   def onthefly_creation_failed(user, auth_source_options = { })
     @user = user
     session[:auth_source_registration] = auth_source_options unless auth_source_options.empty?
-    render :action => 'register'
+    render register_path
   end
 
   def invalid_credentials
@@ -240,7 +240,7 @@ class AccountController < ApplicationController
     if user.save and token.save
       Mailer.register(token).deliver
       flash[:notice] = l(:notice_account_register_done)
-      redirect_to :action => 'login'
+      redirect_to signin_path
     else
       yield if block_given?
     end
@@ -277,6 +277,6 @@ class AccountController < ApplicationController
 
   def account_pending
     flash[:notice] = l(:notice_account_pending)
-    redirect_to :action => 'login'
+    redirect_to signin_path
   end
 end

app/views/account/login.html.erb
@@ -1,6 +1,6 @@
 <%= call_hook :view_account_login_top %>
 <div id="login-form">
-<%= form_tag({:action=> "login"}) do %>
+<%= form_tag(signin_path) do %>
 <%= back_url_hidden_field_tag %>
 <table>
 <tr>
@@ -28,7 +28,7 @@
 <tr>
     <td align="left">
         <% if Setting.lost_password? %>
-            <%= link_to l(:label_password_lost), :controller => 'account', :action => 'lost_password' %>
+            <%= link_to l(:label_password_lost), lost_password_path %>
         <% end %>
     </td>
     <td align="right">

app/views/account/lost_password.html.erb
@@ -1,11 +1,11 @@
 <h2><%=l(:label_password_lost)%></h2>
 
-<div class="box">
-<%= form_tag({:action=> "lost_password"}, :class => "tabular") do %>
-
-<p><label for="mail"><%=l(:field_mail)%> <span class="required">*</span></label>
-<%= text_field_tag 'mail', nil, :size => 40 %>
-<%= submit_tag l(:button_submit) %></p>
-
+<%= form_tag(lost_password_path) do %>
+  <div class="box tabular">
+    <p>
+      <label for="mail"><%=l(:field_mail)%> <span class="required">*</span></label>
+      <%= text_field_tag 'mail', nil, :size => 40 %>
+      <%= submit_tag l(:button_submit) %>
+    </p>
+  </div>
 <% end %>
-</div>

app/views/account/register.html.erb
@@ -1,6 +1,6 @@
 <h2><%=l(:label_register)%> <%=link_to l(:label_login_with_open_id_option), signin_url if Setting.openid? %></h2>
 
-<%= labelled_form_for @user, :url => {:action => 'register'} do |f| %>
+<%= labelled_form_for @user, :url => register_path do |f| %>
 <%= error_messages_for 'user' %>
 
 <div class="box tabular">

config/routes.rb
@@ -20,8 +20,8 @@ RedmineApp::Application.routes.draw do
 
   match 'login', :to => 'account#login', :as => 'signin'
   match 'logout', :to => 'account#logout', :as => 'signout'
-  match 'account/register', :to => 'account#register', :via => [:get, :post]
-  match 'account/lost_password', :to => 'account#lost_password', :via => [:get, :post]
+  match 'account/register', :to => 'account#register', :via => [:get, :post], :as => 'register'
+  match 'account/lost_password', :to => 'account#lost_password', :via => [:get, :post], :as => 'lost_password'
   match 'account/activate', :to => 'account#activate', :via => :get
 
   match '/news/preview', :controller => 'previews', :action => 'news', :as => 'preview_news'

lib/redmine.rb
@@ -162,7 +162,7 @@ end
 
 Redmine::MenuManager.map :account_menu do |menu|
   menu.push :login, :signin_path, :if => Proc.new { !User.current.logged? }
-  menu.push :register, { :controller => 'account', :action => 'register' }, :if => Proc.new { !User.current.logged? && Setting.self_registration? }
+  menu.push :register, :register_path, :if => Proc.new { !User.current.logged? && Setting.self_registration? }
   menu.push :my_account, { :controller => 'my', :action => 'account' }, :if => Proc.new { User.current.logged? }
   menu.push :logout, :signout_path, :if => Proc.new { User.current.logged? }
 end

commit 8841bdb12f5715871255a3481a01685cada03cca

Fixed #onthefly_creation_failed broken by r9940 (redmine-11850).
git-svn-id: https://svn.redmine.org/redmine/trunk@10412 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/account_controller.rb
@@ -238,7 +238,7 @@ class AccountController < ApplicationController
   def onthefly_creation_failed(user, auth_source_options = { })
     @user = user
     session[:auth_source_registration] = auth_source_options unless auth_source_options.empty?
-    render register_path
+    render :action => 'register'
   end
 
   def invalid_credentials