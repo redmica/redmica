ecd0c70a4894ee42a6b4eb4983c20d5cc2cd1215
ishikawa999 <remember929@gmail.com>
2021-06-07 02:33:01 +0000

    Update Bleuclair theme(b731632)

--- public/themes/bleuclair/src/index.js
+++
@@ -1,5 +0,0 @@
-// When built, it will be output as bleuclair/javascripts/theme.js
-import './scripts/bleuclair.js'
-
-// When built, it will be output as bleuclair/stylesheets/theme.css
-import './styles/bleuclair.scss';
