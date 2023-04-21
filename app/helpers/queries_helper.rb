redmica/redmica

commit 90149788512c73620bf971bd9405b9e5d18c114c

Reorder groups in the filter select list (redmine-38402).
Patch by Go MAEDA.

git-svn-id: https://svn.redmine.org/redmine/trunk@22166 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/helpers/queries_helper.rb
@@ -24,7 +24,7 @@ module QueriesHelper
 
   def filters_options_for_select(query)
     ungrouped = []
-    grouped = {}
+    grouped = {label_string: [], label_date: [], label_time_tracking: [], label_attachment: []}
     query.available_filters.map do |field, field_options|
       if field_options[:type] == :relation
         group = :label_relations

commit a132f63b7d056e78a5fb30c4f9436bf8fbbba072

Fixed an issue where an empty group could appear in the filter selection list caused by r22166 (redmine-38402).
git-svn-id: https://svn.redmine.org/redmine/trunk@22172 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/helpers/queries_helper.rb
@@ -52,6 +52,8 @@ module QueriesHelper
         ungrouped << [field_options[:name], field]
       end
     end
+    # Remove empty groups
+    grouped.delete_if {|k, v| v.empty?}
     # Don't group dates if there's only one (eg. time entries filters)
     if grouped[:label_date].try(:size) == 1
       ungrouped << grouped.delete(:label_date).first