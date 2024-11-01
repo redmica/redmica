471e1ed4383b3d69a1512be912fddb341165c815
MAEDA Go <maeda@farend.jp>
2024-11-01 14:15:45 +0900

    Remove .circleci directory

--- .circleci/files/skip_test.rb
+++
@@ -1,9 +0,0 @@
-# Skip those that give an error due to date
-UTC_SKIP_TESTS = {}
-TOKYO_SKIP_TESTS = {"unit/mailer_test.rb" => %w(test_reminder_should_include_issues_assigned_to_groups test_reminders_for_users test_reminders test_reminders_should_sort_issues_by_due_date test_reminders_language_auto),"functional/issues_controller_test.rb" => %w(test_index_grouped_by_created_on_if_time_zone_is_nil)}
-SAMOA_SKIP_TESTS = {"unit/mailer_test.rb" => %w(test_reminder_should_include_issues_assigned_to_groups test_reminders_for_users test_reminders test_reminders_should_sort_issues_by_due_date test_reminders_language_auto)}
-TEST_DIRECTORY_PATH = ARGV[1] || "../../test" 
-Object.const_get(ARGV[0].upcase).each do |path, test_names|
-     content = File.read("#{TEST_DIRECTORY_PATH}/#{path}").gsub(/def setup\n/, "def setup\nskip \'SKIP\' if #{test_names}.include?(self.name)\n")
-File.write("#{TEST_DIRECTORY_PATH}/#{path}", content)
-end
