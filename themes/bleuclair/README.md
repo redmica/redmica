7eb368c8fb93cac296321ff1b44890237271352f
MAEDA Go <maeda@farend.jp>
2024-11-15 13:27:42 +0900

    Update Bleuclair theme

--- themes/bleuclair/README.md
+++ themes/bleuclair/README.md
@@ -19,7 +19,7 @@ Bleuclair theme is used as standard for [RedMica - the future Redmine you can ge
 Storybook([redmine-theme/storybook](https://github.com/redmine-theme/storybook)): https://farend.github.io/redmine_theme_farend_bleuclair
 
 - Issue list
-<kbd><img src="https://github.com/farend/redmine_theme_farend_bleuclair/blob/images/issues-4.2.png" /></kbd>
+<kbd><img src="https://github.com/farend/redmine_theme_farend_bleuclair/blob/images/issues-6.0.png" /></kbd>
 
 ## Install
 
@@ -51,12 +51,12 @@ $ git clone -b redmine4.2 https://github.com/farend/redmine_theme_farend_bleucla
 
 - For Redmine 5.0, Redmine 5.1
 ```bash
-$ git clone https://github.com/farend/redmine_theme_farend_bleuclair.git public/themes/bleuclair
+$ git clone -b redmine5.1 https://github.com/farend/redmine_theme_farend_bleuclair.git public/themes/bleuclair
 ```
 
-- For RedMica 3.0, Redmine trunk
+- For Redmine 6.0, Redmine trunk
 ```bash
-$ git clone -b support-propshaft https://github.com/farend/redmine_theme_farend_bleuclair.git themes/bleuclair
+$ git clone https://github.com/farend/redmine_theme_farend_bleuclair.git themes/bleuclair
 ```
 
 **In an environment where Git is not available:**
--- themes/bleuclair/javascripts/theme.js
+++ themes/bleuclair/javascripts/theme.js
@@ -1,7 +1,7 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 935:
+/***/ 648:
 /***/ (function() {
 
 function setDiscontinuityClassToTabContent(id) {
@@ -39,93 +39,6 @@ $(function () {
       setDiscontinuityClassToTabContent($(this).attr('id'));
     });
   });
-});
-
-// Function based on https://www.redmine.org/issues/21808#note-27 patch.
-// collapsible sidebar jQuery plugin
-(function ($) {
-  // main container this is applied to
-  var main;
-  // triggers show/hide
-  var button;
-  // the key to use in local storage
-  // this will later be expanded using the current controller and action to
-  // allow for different sidebar states for different pages
-  var localStorageKey;
-  // true if local storage is available
-  var canUseLocalStorage = function () {
-    try {
-      if ('localStorage' in window) {
-        localStorage.setItem('redmine.test.storage', 'ok');
-        var item = localStorage.getItem('redmine.test.storage');
-        localStorage.removeItem('redmine.test.storage');
-        if (item === 'ok') return true;
-      }
-    } catch (err) {}
-    return false;
-  }();
-  // function to set current sidebar state
-  var setState = function setState(state) {
-    if (canUseLocalStorage) {
-      localStorage.setItem(localStorageKey, state);
-    }
-  };
-  var applyState = function applyState() {
-    if (main.hasClass('visible-sidebar')) {
-      setState('visible');
-    } else {
-      setState('hidden');
-    }
-  };
-  var setupToggleButton = function setupToggleButton() {
-    button = $('#sidebar-switch-button');
-    button.click(function (e) {
-      main.addClass("animate");
-      main.toggleClass('visible-sidebar');
-      applyState();
-      e.preventDefault();
-      return false;
-    });
-    applyState();
-  };
-  $.fn.collapsibleSidebar = function () {
-    main = this;
-    // determine previously stored sidebar state for this page
-    if (canUseLocalStorage) {
-      // determine current controller/action pair and use them as storage key
-      var bodyClass = $('body').attr('class');
-      if (bodyClass) {
-        try {
-          localStorageKey = 'redmine-sidebar-state-' + bodyClass.split(/\s+/).filter(function (s) {
-            return s.match(/(action|controller)-.*/);
-          }).sort().join('-');
-        } catch (e) {
-          // in case of error (probably IE8), continue with the default key.
-          localStorageKey = 'redmine-sidebar-state';
-        }
-      }
-      var storedState = localStorage.getItem(localStorageKey);
-      main.toggleClass('visible-sidebar', storedState === 'visible' || !storedState);
-    } else {
-      main.toggleClass('visible-sidebar', true);
-    }
-    // draw the toggle button once the DOM is complete
-    $(document).ready(setupToggleButton);
-  };
-})(jQuery);
-window.addEventListener('DOMContentLoaded', function () {
-  if (!$('#main').hasClass('nosidebar')) {
-    if ($('#sidebar-switch-panel').length == 0) {
-      $('#content').prepend('<div id="sidebar-switch-panel"><a id="sidebar-switch-button" href="#"></a></div>');
-    }
-    try {
-      $('#main').collapsibleSidebar();
-    } catch (e) {
-      $('#main').toggleClass('visible-sidebar', true);
-      $('div#sidebar-switch-panel').remove();
-      console.error(e);
-    }
-  }
 });
 
 /***/ })
@@ -187,11 +100,10 @@ window.addEventListener('DOMContentLoaded', function () {
 /******/ 	}();
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be in strict mode.
 !function() {
 "use strict";
-/* harmony import */ var _scripts_bleuclair_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(935);
+/* harmony import */ var _scripts_bleuclair_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(648);
 /* harmony import */ var _scripts_bleuclair_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scripts_bleuclair_js__WEBPACK_IMPORTED_MODULE_0__);
 // When built, it will be output as bleuclair/javascripts/theme.js
 
