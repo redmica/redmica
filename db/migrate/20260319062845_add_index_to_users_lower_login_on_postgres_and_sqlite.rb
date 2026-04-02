52bbedb0d0682f71729b6fd08a5bd34faa66fe65
Marius Balteanu <marius.balteanu@zitec.com>
2026-04-02 19:42:57 +0000

    Add / remove indexes only if exists / not exists (redmine-43838).
    git-svn-id: https://svn.redmine.org/redmine/trunk@24550 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20260319062845_add_index_to_users_lower_login_on_postgres_and_sqlite.rb
+++ db/migrate/20260319062845_add_index_to_users_lower_login_on_postgres_and_sqlite.rb
@@ -1,13 +1,11 @@
 class AddIndexToUsersLowerLoginOnPostgresAndSqlite < ActiveRecord::Migration[8.1]
   def up
     if Redmine::Database.postgresql? || Redmine::Database.sqlite?
-      add_index :users, "(lower(login))", name: "index_users_on_lower_login"
+      add_index :users, "(lower(login))", name: "index_users_on_lower_login", if_not_exists: true
     end
   end
 
   def down
-    if index_exists?(:users, name: "index_users_on_lower_login")
-      remove_index :users, name: "index_users_on_lower_login"
-    end
+    remove_index :users, name: "index_users_on_lower_login", if_exists: true
   end
 end
--- db/migrate/20260319170822_add_index_to_users_login.rb
+++ db/migrate/20260319170822_add_index_to_users_login.rb
@@ -1,9 +1,9 @@
 class AddIndexToUsersLogin < ActiveRecord::Migration[8.1]
   def up
-    add_index :users, :login
+    add_index :users, :login, if_not_exists: true
   end
 
   def down
-    remove_index :users, :login
+    remove_index :users, :login, if_exists: true
   end
 end
